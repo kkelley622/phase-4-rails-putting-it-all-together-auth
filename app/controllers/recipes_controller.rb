e89ae74b7e2e7313c477b64f85ceefb4ccd88c22
Kevin Kelley <kkelley622@gmail.com>
2023-03-27 22:53:34 -0500

    commit tree 401971c5ae7ba5147dccb52f6bae6e90a04f8de1

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,21 @@
+class RecipesController < ApplicationController
+
+    def index 
+        recipes = Recipe.all 
+        render json: recipes, include: [:user], status: 201
+    end
+
+    def create 
+        recipe = @current_user.recipes.create!(recipe_params)
+        render json: recipe, include: [:user], status: 201
+    end
+
+    private
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+
+end
+
+
